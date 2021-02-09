73e078650e14cb8ba73bd25519da9fb9a30c1414
Jose Mendoza <pball626@gmail.com>
2021-02-09 19:13:29 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,9 +2,9 @@ require "pry"
 def join_nested_strings(src)
 strings = [ ]
 row_index = 0 
-while row_index < src.count do 
+while row_index < src.length do 
   element_index = 0 
-  while element_index < src[row_index].count do 
+  while element_index < src[row_index].length do 
     if src[row_index][element_index].class == String
       strings << src[row_index][element_index]
     end
